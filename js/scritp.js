51aa98d89dc6c8bc4a9486f537bee87a9f2fe951
noxopio <dark.tempus@gmail.com>
2023-02-02 15:39:44 -0500

    sass4

---
+++ js/scritp.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+let LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ*!";
+document.querySelector("h1").onmouseover = Event => {
+    let interations = 0;
+    const interval = setInterval(() => {
+        Event.target.innerText = Event.target.innerText.split("").map((letter, index) => {
+            if (index < interations) {
+                return Event.target.dataset.value[index];
+            }
+            return LETTERS[Math.floor(Math.random() * 28)]
+        }).join("");
+        if (interations >= Event.target.dataset.value.length) {
+            clearInterval(interval);
+        }
+        interations+=1/3;
+    }, 40)
+};
+
+
+ LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ*!";
+document.querySelector("h2").onmouseover = Event => {
+    let interations = 0;
+    const interval = setInterval(() => {
+        Event.target.innerText = Event.target.innerText.split("").map((letter, index) => {
+            if (index < interations) {
+                return Event.target.dataset.value[index];
+            }
+            return LETTERS[Math.floor(Math.random() * 28)]
+        }).join("");
+        if (interations >= Event.target.dataset.value.length) {
+            clearInterval(interval);
+        }
+        interations+=1/3;
+    }, 40)
+};
