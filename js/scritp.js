24cf5616910d94541885d2bb478df723673d3dce
noxopio <DARK.TEMPUS@GMAIL.COM>
2023-02-02 21:39:39 -0500

    iframe\styles2

--- js/scritp.js
+++ js/scritp.js
@@ -22,7 +22,7 @@ document.querySelector("h1").onmouseover = Event => {
 
 
  LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ*!";
-document.querySelector("h2").onmouseover = Event => {
+document.querySelector("h").onmouseover = Event => {
     let interations = 0;
     const interval = setInterval(() => {
         Event.target.innerText = Event.target.innerText.split("").map((letter, index) => {
@@ -38,19 +38,3 @@ document.querySelector("h2").onmouseover = Event => {
     }, 40)
 };
 
-
-document.querySelector("h1").onmouseover = Event => {
-    let interations = 0;
-    const interval = setInterval(() => {
-        Event.target.innerText = Event.target.innerText.split("").map((letter, index) => {
-            if (index < interations) {
-                return Event.target.dataset.value[index];
-            }
-            return LETTERS[Math.floor(Math.random() * 28)]
-        }).join("");
-        if (interations >= Event.target.dataset.value.length) {
-            clearInterval(interval);
-        }
-        interations+=1/3;
-    }, 40)
-};
